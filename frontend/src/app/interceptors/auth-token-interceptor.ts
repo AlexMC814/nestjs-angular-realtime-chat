7188b2f653ba5297b71758b11c31115858e3b8ef
Aleksandr Musarov <musarov@cifra-broker.ru>
2024-09-05 16:22:04 +0300

    Added authorization token interceptor to pass jwt token in request headers

--- frontend/src/app/interceptors/auth-token-interceptor.ts
+++ frontend/src/app/interceptors/auth-token-interceptor.ts
@@ -1,10 +1,15 @@
-import { HttpEvent, HttpHandlerFn, HttpRequest } from "@angular/common/http";
-import { Injectable } from "@angular/core";
-import { Observable } from "rxjs";
-import { tokenGetter } from "../app.config";
+import { HttpEvent, HttpHandlerFn, HttpRequest } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { tokenGetter } from '../app.config';
 
-export function authTokenInterceptor(req: HttpRequest<unknown>, next: HttpHandlerFn): Observable<HttpEvent<any>> {
-    const token = tokenGetter();
-    const newReq = req.clone({ headers: req.headers.append('Authorization', `Bearer ${token}`) });
-    return next(newReq);
+export function authTokenInterceptor(
+  req: HttpRequest<unknown>,
+  next: HttpHandlerFn
+): Observable<HttpEvent<any>> {
+  const token = tokenGetter();
+  const newReq = req.clone({
+    headers: req.headers.append('Authorization', `Bearer ${token}`),
+  });
+  return next(newReq);
 }
